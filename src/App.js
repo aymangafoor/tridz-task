00c24db7f98d4ec475fb252ccbfd896fff12e1af
aymangafoor <aymengafoorn@gmail.com>
2020-11-24 15:07:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactPlayer from "react-player";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Quote from './Quote'
+import './App.css'
+import play from './images/play.png'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div className='tab1'>
+            <Link className='link' to="/trading">Trading Division</Link>
+            <Link className='link' to="/project">Projects Division</Link>
+            <Link className='link' to="/switch">Switchgears Division</Link>
+            <Link className='linkquote' to="/getquote">Get a Quote</Link>
+          </div>
+        </BrowserRouter>
+        <BrowserRouter>
+          <div className='tab2'>
+            <Link className='link2' to="/trading">Electricals</Link>
+            <Link className='link2' to="/project">Building {'&'} Construction Materials</Link>
+            <Link className='link2' to="/switch">Plumbing {'&'} Fitings</Link>
+            <Link className='link2' to="/getquote">Tiles</Link>
+            <Link className='link2' to="/getquote">Paints {'&'} Accessories</Link>
+            <Link className='link2' to="/getquote">Tools</Link>
+            <Link className='link2' to="/getquote">Hardware</Link>
+            <Link className='link2' to="/getquote">Fancy Lights</Link>
+            <Link className='link2' to="/getquote">Furniture</Link>
+          </div>
+          <div className='center'>
+            <Switch>
+              <Route path='/getquote' component={Quote} />
+            </Switch>
+          </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </BrowserRouter>
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            light={true}
+            controls
+            playIcon={<img src={play} alt='play'/>}
+            url="https://www.youtube.com/watch?v=iYn9fFQoRJU"
+          />
+        </div>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Quote.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './Quote.css'
+class Quote extends Component {
+    constructor(props){
+        super(props)
+    this.state={
+        name: null,
+        email:null,
+        phone:null,
+        section:null,
+        storage:null,
+        description:null
+
+    }
+    this.store = this.store.bind(this);
+    this.show = this.show.bind(this);
+
+}
+    store(event){
+        this.setState({
+            [event.target.name]: event.target.value,
+            
+        });
+    }
+    show(){
+        alert("name:"+this.state.name+", email:"+this.state.email+" phone:"+this.state.phone+", section:"+this.state.section+" ,storage:"+this.state.storage+" ,description:"+this.state.description);
+    }
+    render() {
+        return (
+            <div className='quote'>
+                <form onSubmit={this.show}>
+                <a href='/' className='back'>X</a>
+                <label className='heading'>Get a quote</label>
+                <div className='firstrow'>
+                    <input type='text' value={this.state.name} name='name' className='first' placeholder='Name'
+                     onChange={this.store}></input>
+                    <input type='text' value={this.state.email} name='email' className='first' placeholder='Email'
+                    onChange={this.store}></input>
+                    <input type='text' value={this.state.phone} name='phone' className='first' placeholder='Phone'
+                    onChange={this.store}></input>
+                </div>
+                <div className='select'>
+                    <select defaultValue='Section' value={this.state.section} name='section' className='selects' placeholder='Section'
+                    onChange={this.store}>
+                        <option value="none">Section</option>
+                        <option value="section A">Section A</option>
+                        <option value="section B">Section B</option>
+                        <option value="section C">Section C</option>
+                    </select>
+                    <select className='selects' value={this.state.storage} name='storage' placeholder='Store Area'>
+                        <option value="none">Storage Area</option>
+                        <option value="Storage Area 1">Storage Area 1</option>
+                        <option value="Storage Area 2">Storage Area 2</option>
+                    </select>
+                </div>
+                <div className='last'>
+                <input type='textarea' value={this.state.description} name='description' className='requirement' placeholder='Requirement'></input>
+                </div>
+            <input type='submit' className='submit' value='Send'></input>
+                </form>
+            </div>
+        )
+    }
+} export default Quote
